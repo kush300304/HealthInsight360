9d03ae6e737c226d513b1ba17c110f544bfc745d
kush300304 <131548860+kush300304@users.noreply.github.com>
2024-06-01 19:42:11 +0530

    Update settings.py

--- healthinsight/settings.py
+++ healthinsight/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3q)=ni@=e!!88!q0@##1rqddplk2h3_9*2f33koc&m4=)bai*x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
